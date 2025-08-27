1a33fdf0b4ef8eb2ac8797fabec79f8f757ee71f
Luis-Herique <luis1416henrique@gmail.com>
2025-08-27 12:35:58 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import flask, request, render_template
+
+app = flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pag1')
+def index():
+    return render_template('pag1.html')
+
+@app.route('/pag2')
+def index():
+    return render_template('pag2.html')
+
+@app.route('/pag3')
+def index():
+    return render_template('pag3.html')
+
+if __name__ == '__main__':
+    app.run(debug=true)
